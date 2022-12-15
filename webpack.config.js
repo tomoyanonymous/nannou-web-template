d4e5309c5ea0b7b685917c2233dd796e7cdd21aa
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2022-12-15 20:55:26 +0900

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const webpack = require("webpack");
 
 const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
   mode: "production",
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  module: {
+    rules: [{
+      test: /\.wasm$/,
+      type: "webassembly/async"
+    }]
+  },
   entry: {
     index: "./js/index.js"
   },
@@ -14,15 +24,25 @@ module.exports = {
     filename: "[name].js"
   },
   devServer: {
-    contentBase: dist,
+    static: [dist],
   },
+  performance: { hints: false },
   plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
+    new CopyPlugin({
+      patterns: [
+        path.resolve(__dirname, "static")
+      ]
+    }),
 
     new WasmPackPlugin({
       crateDirectory: __dirname,
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        experiments: {
+          asyncWebAssembly: true
+        }
+      }
     }),
   ]
 };
